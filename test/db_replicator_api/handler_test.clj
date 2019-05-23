07c61f811bb820e81d9ed3515b27ed62d31eb49a
= <rogeramorinvieiraa@gmail.com>
2019-05-23 16:12:27 -0300

    remove some broken tests

--- test/db_replicator_api/handler_test.clj
+++ test/db_replicator_api/handler_test.clj
@@ -4,14 +4,6 @@
             [db-replicator-api.handler :refer :all]))
 
 
-(facts "Hello World on root"
-   (fact "response status is 200"
-      (let [response (app (mock/request :get "/"))]
-  				(:status response) => 200))
-   (fact "response body is hello world"
-      (let [response (app (mock/request :get "/"))]
-         (:body response) => "Hello World")))
-
 (facts "Invalid Route"
    (fact "response status is 404"
       (let [response (app (mock/request :get "/invalid"))]
