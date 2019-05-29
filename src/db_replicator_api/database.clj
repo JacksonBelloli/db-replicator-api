f3567782f21e37e9627d40c6aa36942bd1546092
= <rogeramorinvieiraa@gmail.com>
2019-05-29 16:08:39 -0300

    delete elements that have in the destin and dont have in origin

--- src/db_replicator_api/database.clj
+++ src/db_replicator_api/database.clj
@@ -7,19 +7,22 @@
 	(jdbc/query database
 		(sql/select * table-name)))
 
-(defn db-insert!
-	[database table-name data]
-	(jdbc/insert! database table-name data))
-
-(defn db-insert-multi!
-	[database table data]
-	(map db-insert! data))
-
 (defn db-select-all-where
 	[database table-name conditions]
 	(jdbc/query database
 		(sql/select * table-name
 			(sql/where conditions))))
+
+(defn db-select-all-where-order
+	[database table-name conditions order]
+	(jdbc/query database
+		(sql/select * table-name
+			(sql/where conditions)
+			(sql/order-by order))))
+
+(defn db-insert!
+	[database table-name data]
+	(jdbc/insert! database table-name data))
 
 (defn db-update-where!
 	[database table-name data conditions]
@@ -27,6 +30,4 @@
 
 (defn db-delete-where
 	[database table-name conditions]
-	(jdbc/query database
-		(sql/delete table-name
-			(sql/where conditions))))
+	(jdbc/delete! database table-name (sql/where conditions)))
--- src/db_replicator_api/replicator.clj
+++ src/db_replicator_api/replicator.clj
@@ -20,25 +20,35 @@
 
 (defn get-table-order
    [core-db process-id]
-   (db-select-all-where core-db :OrderProcess {:id_process process-id}))
+   (db-select-all-where-order core-db :OrderProcess {:id_process process-id} :order_process))
 
 (defn get-direction-information
    [core-db direction]
       [(get-db core-db (:id_origin direction))
       (get-db core-db (:id_destin direction))])
 
+(defn remove-extra-elements
+   [index order core-db destin destin-elements]
+   (if (< index (count destin-elements))
+      (println "Removendo um elemento")
+      (let [key-name (keyword (get order :key_name))]
+         (db-delete-where destin (get order :table_destin)
+               {key-name (get (nth destin-elements index) key-name)}))))
+
 (defn execute-changes
    [order core-db origin destin element]
-   (println "Replicando um elemento")
    (let [key-name (keyword (get order :key_name))]
       (let [contitions {key-name (get element key-name)} table (get order :table_destin)]
          (if (= 0 (count (db-select-all-where destin table contitions)))
-               (db-insert! destin table element)
-               (db-update-where! destin table element contitions)))))
+               (do
+                  (println "Inserindo novo elemento")
+                  (db-insert! destin table element))
+               (do
+                  (println "Atualizando um elemento")
+                  (db-update-where! destin table element contitions))))))
 
 (defn execute-elements
    [index order core-db origin destin origin-elements destin-elements]
-   (println index)
    (if (< index (count origin-elements))
       (let [element-origin (nth origin-elements index)
             key-name (keyword (get order :key_name))]
@@ -46,12 +56,14 @@
             (do
                (execute-changes order core-db origin destin element-origin)
                (recur (inc index) order core-db origin destin origin-elements
-                  (remove #(= (get % key-name) (get element-origin key-name)) destin-elements)))
-            (recur (inc index) order core-db origin destin origin-elements destin-elements)))))
-
+                     (remove #(= (get % key-name) (get element-origin key-name)) destin-elements)))
+            (recur (inc index) order core-db origin destin origin-elements
+                  (remove #(= (get % key-name) (get element-origin key-name)) destin-elements))))
+      (remove-extra-elements 0 order core-db destin destin-elements)))
 
 (defn execute-table
    [order core-db process direction origin destin]
+   (println "Replicando tabela: " (get order :table_origin) "->" (get order :table_destin))
    (execute-elements 0 order core-db origin destin
                      (db-select-all origin (get order :table_origin))
                      (db-select-all destin (get order :table_destin))))
@@ -63,20 +75,19 @@
                (get-table-order core-db id)
                (get-db-direction core-db id))))
    ([core-db process order direction]
-      (execute core-db process order direction (get-direction-information core-db direction)))
-   ([core-db process order direction [origin destin]]
-      (println "Inciando replicacao de tabelas...")
-      (loop [x (- (count order) 1)]
-         (when (> x -1)
-            (let [db_origin (generate-db (:db_type origin) (:name origin)
-                                          (:ip origin) (:port origin)
-                                          (:user_origin direction) (:password_origin direction))
-                  db_destin (generate-db (:db_type destin) (:name destin)
-                                                (:ip destin) (:port destin)
-                                                (:user_destin direction) (:password_destin direction))]
-                  (execute-table (nth order x) core-db process direction db_origin db_destin)
-            (recur (- x 1)))))))
+      (execute 0 core-db process order direction (get-direction-information core-db direction)))
+   ([index core-db process order direction [origin destin]]
+      (if (< index (count order))
+         (let [db_origin (generate-db (:db_type origin) (:name origin)
+                                       (:ip origin) (:port origin)
+                                       (:user_origin direction) (:password_origin direction))
+               db_destin (generate-db (:db_type destin) (:name destin)
+                                             (:ip destin) (:port destin)
+                                             (:user_destin direction) (:password_destin direction))]
+               (execute-table (nth order index) core-db process direction db_origin db_destin)
+         (recur (inc index) core-db process order direction [origin destin])))))
 
 (defn init
    [core-db process-id]
+   (println "Inciando replicacao de tabelas...")
    (execute core-db (first (db-select-all-where core-db :Process {:id process-id}))))
